0bdf0e466a0fde390327279766b4008a042d5638
fathimahanaan <hanaanfh764@icloud.com>
2025-02-26 17:12:09 +0530

    just pushing the files-2

--- controllers/newController.js
+++ controllers/newController.js
@@ -10,3 +10,21 @@ export const addSubCommunity = async (req, res) => {
   await newSubCommunity.save();
   res.status(201).json({ message: "successfully created" });
 };
+
+export const updateModule = async (req, res) => {
+  const { moduleName, relatedCourse } = req.body;
+  const { id } = req.params;
+  const module = await Module.findById(id);
+  if (!module) throw new NotFoundError("module not found");
+  module.moduleName = moduleName;
+  module.relatedCourse = relatedCourse;
+  await module.save();
+  res.status(200).json({ message: " module is updated" });
+};
+
+export const getSingleSubCommunity = async (req, res) => {
+  const { id } = req.params;
+  const subCommunity = await SubCommunity.findById(id);
+  if (!subCommunity) throw new NotFoundError("no community found");
+  res.status(200).json(subCommunity);
+};

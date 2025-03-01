d9edbf98feca1f76c7995b9280fffa24f1449394
fathimahanaan <hanaanfh764@icloud.com>
2025-03-01 12:29:06 +0530

    route changed for flutter

--- middleware/authenticationMiddleware.js
+++ middleware/authenticationMiddleware.js
@@ -7,9 +7,9 @@ import { verifyJWT } from "../utils/jwtUtils.js";
 export const authenticateUser = async (req, res, next) => {
   //middleware for decoding the token, middleware for checking if the user has valid token or not
 
-  // const token = req.headers.authorization?.split(" ")[1]; //for flutter
+  const token = req.headers.authorization?.split(" ")[1]; //for flutter
   // changed this route
-  const { token } = req.cookies; //for web-apps
+  // const { token } = req.cookies; //for web-apps
   if (!token) throw new UnauthenticatedError("unable to access");
   try {
     const decoded = verifyJWT(token);

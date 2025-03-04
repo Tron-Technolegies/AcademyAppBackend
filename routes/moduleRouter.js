d01f80d0d8c4cd2b2a35d0bda5502e747980f117
fathimahanaan <hanaanfh764@icloud.com>
2025-03-04 11:09:21 +0530

    added populate for get all course

--- routes/moduleRouter.js
+++ routes/moduleRouter.js
@@ -1,9 +1,13 @@
 import { Router } from "express";
-import { validateModuleInput } from "../middleware/validationMiddleware.js";
+import {
+  validateModuleByCourseInput,
+  validateModuleInput,
+} from "../middleware/validationMiddleware.js";
 import {
   addModule,
   deleteModule,
   getAllModule,
+  getModuleByCourse,
   getSingleModule,
   updateModule,
 } from "../controllers/moduleController.js";
@@ -15,4 +19,9 @@ router.get("/getModule", getAllModule);
 router.patch("/updateModule/:id", validateModuleInput, isAdmin, updateModule);
 router.get("/getModule/:id", getSingleModule);
 router.delete("/deleteModule/:id", isAdmin, deleteModule);
+router.get(
+  "/getModuleByCourse/:id",
+  validateModuleByCourseInput,
+  getModuleByCourse
+);
 export default router;

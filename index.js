1745120bf0406fe14b45cb76c5606307dcdc4eb0
clawlynx <clawlynx77@gmail.com>
2025-04-21 16:26:57 +0530

    added the cors origin of backened

--- index.js
+++ index.js
@@ -39,7 +39,11 @@ cloudinary.config({
 
 const _dirname = dirname(fileURLToPath(import.meta.url));
 
-const allowedOrigins = ["http://localhost:5173", "http://localhost:5174"];
+const allowedOrigins = [
+  "http://localhost:5173",
+  "http://localhost:5174",
+  "https://api.tronacademy.in",
+];
 
 app.use(
   cors({

4d137d6f4fd9657d5ec7d7b46977044fa4ac9dff
fathimahanaan <hanaanfh764@icloud.com>
2025-06-12 12:14:46 +0100

    quiz

--- controllers/quizController.js
+++ controllers/quizController.js
@@ -83,92 +83,118 @@ export const getQuizByModule = async (req, res) => {
   res.status(200).json(quizzes);
 };
 
-// export const calculateQuizScore = async (req, res) => {
-//   const { quizId, userAnswers } = req.body;
-//   const quiz = await Quiz.findById(quizId);
-//   if (!quiz) throw new NotFoundError("Quiz not found");
+export const calculateQuizScore = async (req, res) => {
+  const { id } = req.params; //quiz id
+  const { userAnswers } = req.body;
 
-//   const questions = quiz.questions;
-//   let correctAnswers = 0;
+  const quiz = await Quiz.findById(id);
+  if (!quiz) throw new NotFoundError("Quiz not found");
 
-//   questions.forEach((question, index) => {
-//     if (userAnswers[index] === question.answer) {
-//       correctAnswers++;
-//     }
-//   });
+  let correctAnswers = 0;
 
-//   const totalQuestions = questions.length;
-//   const scorePercentage = (correctAnswers / totalQuestions) * 100;
+  userAnswers.forEach(({ questionId, answer }) => {
+    const question = quiz.questions.find(
+      (q) => q._id.toString() === questionId
+    );
+    if (question && question.answer === answer) {
+      correctAnswers++;
+    }
+  });
 
-//   res.status(200).json({
-//     correctAnswers,
-//     totalQuestions,
-//     scorePercentage,
-//   });
-// };
+  const totalQuestions = quiz.questions.length;
+  const scorePercentage = (correctAnswers / totalQuestions) * 100;
 
-// export const submitQuiz = async (req, res) => {
-//   const { quizId, userAnswers, timeTaken } = req.body;
-//   const { userId } = req.user.userId;
+  res.status(200).json({ correctAnswers, totalQuestions, scorePercentage });
+};
 
-//   // Fetch quiz with correct answers
-//   const quiz = await Quiz.findById(quizId);
-//   if (!quiz) throw new NotFoundError("Quiz not found");
+export const submitQuizResult = async (req, res) => {
+  const { quizId, userAnswers, timeTaken } = req.body;
+  const userId = req.user.userId;
 
-//   let correctAnswers = 0;
-//   const totalQuestions = quiz.questions.length;
+  // Fetch quiz with correct answers
+  const quiz = await Quiz.findById(quizId);
+  if (!quiz) throw new NotFoundError("Quiz not found");
 
-//   // Compare answers
-//   quiz.questions.forEach((question, index) => {
-//     if (userAnswers[index] === question.answer) {
-//       correctAnswers++;
-//     }
-//   });
+  let correctAnswers = 0;
+  const totalQuestions = quiz.questions.length;
 
-//   // Save the quiz result
-//   const newQuizResult = new QuizResult({
-//     user: userId,
-//     quiz: quizId,
-//     score: correctAnswers,
-//     totalQuestions,
-//     timeTaken,
-//   });
+  // Compare answers
+  userAnswers.forEach(({ questionId, answer }) => {
+    const question = quiz.questions.find(
+      (q) => q._id.toString() === questionId
+    );
+    if (question && question.answer === answer) {
+      correctAnswers++;
+    }
+  });
 
-//   await newQuizResult.save();
-//   res.status(201).json({
-//     message: "Quiz submitted successfully",
-//     score: correctAnswers,
-//     totalQuestions,
-//   });
-// };
+  // Save the quiz result
+  const newQuizResult = new QuizResult({
+    user: userId,
+    quiz: quizId,
+    score: correctAnswers,
+    totalQuestions,
+    timeTaken,
+  });
 
-// export const calculateCourseScore = async (req, res) => {
-//   const { courseId } = req.body;
-//   const { userId } = req.user.userId;
-//   const quizzes = await Quiz.find({ relatedCourse: courseId });
-//   if (!quizzes) throw new NotFoundError("No quizzes found for this course");
+  await newQuizResult.save();
+  res.status(201).json({
+    message: "Quiz submitted successfully",
+    score: correctAnswers,
+    totalQuestions,
+    resultId: newQuizResult._id,
+  });
+};
 
-//   let totalCorrectAnswers = 0;
-//   let totalQuestionsAttempted = 0;
+export const calculateCourseScore = async (req, res) => {
+  const { courseId } = req.params;
+  const userId = req.user.userId;
 
-//   for (const quiz of quizzes) {
-//     const quizResult = await QuizResult.findOne({
-//       user: userId,
-//       quiz: quiz._id,
-//     });
-//     if (quizResult) {
-//       totalCorrectAnswers += quizResult.score;
-//       totalQuestionsAttempted += quizResult.totalQuestions;
-//     }
+  // Find all quizzes related to the course (includes all modules)
+  const quizzes = await Quiz.find({ relatedCourse: courseId });
+  if (!quizzes.length) {
+    throw new NotFoundError("No quizzes found for this course");
+  }
+
+  // Get all quiz IDs from the course's quizzes
+  const quizIds = quizzes.map((q) => q._id);
+
+  // Find all quiz results of this user for those quizzes
+  const quizResults = await QuizResult.find({
+    user: userId,
+    quiz: { $in: quizIds },
+  });
+
+  let totalCorrectAnswers = 0;
+  let totalQuestionsAttempted = 0;
+
+  // Sum up correct answers and total questions attempted for all quizzes
+  quizResults.forEach((result) => {
+    totalCorrectAnswers += result.score;
+    totalQuestionsAttempted += result.totalQuestions;
+  });
+
+  // Calculate overall percentage score for the course
+  const overallScorePercentage =
+    totalQuestionsAttempted > 0
+      ? (totalCorrectAnswers / totalQuestionsAttempted) * 100
+      : 0;
+
+  res.status(200).json({
+    courseId,
+    totalCorrectAnswers,
+    totalQuestionsAttempted,
+    overallScorePercentage,
+  });
+};
+// export const getQuizResults = async (req, res) => {
+//   const userId = req.user.userId;
+
+//   const results = await QuizResult.find({ user: userId }).populate("quiz");
+
+//   if (!results.length) {
+//     return res.status(404).json({ message: "No quiz results found" });
 //   }
-//   const overallScorePercentage =
-//     totalQuestionsAttempted > 0
-//       ? (totalCorrectAnswers / totalQuestionsAttempted) * 100
-//       : 0;
 
-//   res.status(200).json({
-//     totalCorrectAnswers,
-//     totalQuestionsAttempted,
-//     overallScorePercentage,
-//   });
+//   res.status(200).json(results);
 // };
--- routes/quizRouter.js
+++ routes/quizRouter.js
@@ -2,11 +2,15 @@ import { Router } from "express";
 
 import {
   addQuiz,
+  calculateCourseScore,
+  calculateQuizScore,
   deleteQuiz,
   getAllQuiz,
   getQuizByCourse,
   getQuizByModule,
+  getQuizResults,
   getSingleQuiz,
+  submitQuizResult,
   updateQuiz,
 } from "../controllers/quizController.js";
 import { validateQuizInput } from "../middleware/validationMiddleware.js";
@@ -16,11 +20,16 @@ const router = Router();
 
 router.get("/quizByCourse", getQuizByCourse);
 router.get("/quizByModule", getQuizByModule);
+router.get("/courseScore/:courseId", calculateCourseScore); //calculate score total modules in the course
+// router.get("/quizResults/:id", getQuizResults);
+
+router.post("/calculateScore/:id", calculateQuizScore);
+router.post("/submitResult", submitQuizResult);
 
 router.post("/", validateQuizInput, isAdmin, addQuiz);
 router.get("/", getAllQuiz);
 router.patch("/:id", validateQuizInput, isAdmin, updateQuiz);
 router.get("/:id", getSingleQuiz);
-router.delete("/:id", validateQuizInput, isAdmin, deleteQuiz);
+router.delete("/:id", isAdmin, deleteQuiz);
 
 export default router;
